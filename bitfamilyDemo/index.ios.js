e0d407e135321835beca025a4ff36c1872651abb
Sujal Bandhara <sujalbandahra@gmail.com>
2017-07-20 16:01:19 +0530

    Modified Index.ios

--- bitfamilyDemo/index.ios.js
+++ bitfamilyDemo/index.ios.js
@@ -17,7 +17,7 @@ export default class bitfamilyDemo extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! sujal
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
